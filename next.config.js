bcbd622b3f7799c981c15d928e46badd4ab834ef
Giovani Franz <giovanifranz151@gmail.com>
2021-11-20 13:45:56 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  env: {
+    NEXT_API_API_KEY: process.env.NEXT_API_API_KEY,
+    NEXT_API_AUTH_DOMAIN: process.env.NEXT_API_AUTH_DOMAIN,
+    NEXT_API_DATABASE_URL: process.env.NEXT_API_DATABASE_URL,
+    NEXT_API_PROJECT_ID: process.env.NEXT_API_PROJECT_ID,
+    NEXT_API_STORAGE_BUCKET: process.env.NEXT_API_STORAGE_BUCKET,
+    NEXT_API_MESSAGING_SENDER_ID: process.env.NEXT_API_MESSAGING_SENDER_ID,
+    NEXT_API_APP_ID: process.env.NEXT_API_APP_ID,
+  },
 };
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
+  apiKey: process.env.NEXT_API_API_KEY,
+  authDomain: process.env.NEXT_API_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_API_DATABASE_URL,
+  projectId: process.env.NEXT_API_PROJECT_ID,
+  storageBucket: process.env.NEXT_API_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_API_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_API_APP_ID,
 };
 
 const firebase = initializeApp(firebaseConfig);
