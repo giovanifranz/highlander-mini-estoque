853965edd0c7ca6de44b19ef5e3b0ea63f639eb4
Giovani Franz <giovanifranz151@gmail.com>
2021-11-20 13:51:38 -0300

    first commit

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,18 +1,35 @@
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
+import { GetStaticPaths } from "next";
 
 export const DynamicHome = dynamic(() => import("../containers/Home"), {
   loading: () => <p>Loading...</p>,
 });
 
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: {
+          slug: "login",
+        },
+      },
+      {
+        params: {
+          slug: "create-account",
+        },
+      },
+    ],
+    fallback: false,
+  };
+};
+
 export default function Home() {
   const route = useRouter().query.slug;
-  
+
   return (
     <>
-      {route === "login" || route === "create-account" ? (
-        <DynamicHome />
-      ) : null}
+      {route === "login" || route === "create-account" ? <DynamicHome /> : null}
     </>
   );
 }
