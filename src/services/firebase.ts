5a4e15834dedbdf0173a323d0c5dc33b6553d436
Giovani Franz <giovanifranz151@gmail.com>
2021-11-20 13:26:26 -0300

    first commit

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,17 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC91_54tzCXdqOlKsYdXCz5vJSNLWp2IhE",
-  authDomain: "highlander-mini-estoque.firebaseapp.com",
-  databaseURL: "https://highlander-mini-estoque-default-rtdb.firebaseio.com",
-  projectId: "highlander-mini-estoque",
-  storageBucket: "highlander-mini-estoque.appspot.com",
-  messagingSenderId: "821370763060",
-  appId: "1:821370763060:web:d95045c6520dc314ad1761",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 const firebase = initializeApp(firebaseConfig);
-
 const auth = getAuth(firebase);
 const database = getDatabase(firebase);
 
