db84eb0e603900e6bef4e7d3346a2de770f2e9a9
Giovani Franz <giovanifranz151@gmail.com>
2021-11-22 03:28:00 -0300

    envio de email personalizado com qrCode

--- src/services/sendGrid.ts
+++ src/services/sendGrid.ts
@@ -11,12 +11,12 @@ export async function sendEmail(
   accountID: string,
   { email, name, qrCode, inventory }: Data
 ) {
-  if (process.env.NEXT_API_SENDGRID_API_KEY !== undefined) {
+  if (process.env.NEXT_API_SENDGRID_API_KEY !== undefined && process.env.NEXT_PUBLIC_EMAIL !== undefined) {
     sgMail.setApiKey(process.env.NEXT_API_SENDGRID_API_KEY);
 
     const msg = {
       to: email,
-      from: "comercial@highlandertech.com.br",
+      from: process.env.NEXT_PUBLIC_EMAIL,
       subject: `Parabéns ${name}, criou o estoque: ${inventory}`,
       html: `O código do seu estoque é: ${accountID}
       <br><img src=${qrCode} alt="qrCode"/><br> 
